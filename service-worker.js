0b7123193aecaa768f1053206fa2813d5de3abc8
zsj4j2s4df-pixel <zsj4j2s4df@privaterelay.appleid.com>
2026-07-09 20:33:40 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE = "zaagerij-v7";
+const ASSETS = ["./","./index.html","./app.js","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./icon-512-maskable.png"];
+self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
+self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener("fetch", (e) => {
+  const req = e.request; if (req.method !== "GET") return;
+  e.respondWith(caches.match(req).then((r) => r || fetch(req).then((resp) => { const copy = resp.clone(); caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {}); return resp; }).catch(() => caches.match("./index.html"))));
+});
